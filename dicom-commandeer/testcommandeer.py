aef6c8911d284b2014b6648a6ad1c81d5106ecb5
Oerb <bjoern@oerb.de>
2013-09-12 21:33:38 +0200

    dicom-commander all dicomlib Parameter for print added not tested

--- dicom-commandeer/testcommandeer.py
+++ dicom-commandeer/testcommandeer.py
@@ -6,13 +6,54 @@ __copyright__ = "GPL 2013"
 __license__ = "GPL" 
 
 
-def print_data_command(filepath, instanceCreationDate=False, accessionNumber=False):
+def print_data_command(filepath, instanceCreationDate=False, 
+        accessionNumber=False, approvalStatus=False, 
+        beamSequence=False, doseReferenceSequence=False, 
+        fractionGroupSequence=False):
     """ Show dicom Data by given Parameter
 
     some commend foo
-    @param filepath to dicom File
-    @param instanceCreationDate 
+    @param filepath to dicom File 
     @param accessionNumber
+    @param approvalStatus
+    @param beamSequence
+    @param doseReferenceSequence
+    @param fractionGroupSequence
+    @param instanceCreation
+    @param instanceCreationDate
+    @param instanceCreationTime
+    @param institutionName
+    @param institutionalDepartmentName
+    @param manufacturer
+    @param manufacturerModelName
+    @param modality
+    @param operatorsName
+    @param patientBirthDate
+    @param patientID
+    @param patientName
+    @param patientSetupSequence
+    @param patientSex
+    @param RTPlanDate
+    @param RTPlanGeometry
+    @param RTPlanLabel
+    @param RTPlanName
+    @param RTPlanTime
+    @param RefdRTPlanSequence
+    @param RefdStructureSetSequence
+    @param ReferencedRTPlanSequence
+    @param ReferencedStructureSetSequence
+    @param referringPhysicianName
+    @param SOPClassUID
+    @param SOPInstanceUID
+    @param seriesInstanceUID
+    @param seriesNumber
+    @param softwareVersions
+    @param stationName
+    @param studyDate
+    @param studyID
+    @param studyInstanceUID
+    @param studyTime
+ 
     """
     import dicom
     plan = dicom.read_file(filepath)
@@ -21,7 +62,80 @@ def print_data_command(filepath, instanceCreationDate=False, accessionNumber=Fal
         print "InstanceCreationDate: " + plan.InstanceCreationDate
     if accessionNumber:
         print "AccesionNumber: " + plan.AccessionNumber
-
+    if approvalStatus:
+        print "ApprovalStatus: " + plan.ApprovalStatus
+    if beamSequence:
+        print "BeamSequence: " + str(plan.BeamSequence)
+    if doseReferenceSequence:
+        print "DoseReferenceSequence: " + str(plan.DoseReferenceSequence)
+    if fractionGroupSequence:
+        print "FractionGroupSequence: " + str(plan.FractionGroupSequence)
+    if instanceCreationTime:
+        print "InstanceCreationTime: " + plan.InstanceCreationTime
+    if institutionName:
+        print "InstitutionName: " + plan.InstitutionName
+    if manufacturer:
+        print "Manufacturer: " + plan.Manufacturer
+    if manufacturerMode1Name:
+        print "ManufacturerMode1Name: " + plan.ManufacturerMode1Name
+    if modality:
+        print "Modality: " + plan.Modality
+    if operatorsName:
+        print "OperatorsName: " + plan.OperatorsName
+    if patientBirthDate:
+        print "PatientBirthDate: " + plan.PatientBirthDate
+    if patientID:
+        print "PatientID: " + plan.PatientID
+    if patientName:
+        print "PatientName: " + plan.PatientName
+    if patientSetupSequence:
+        print "PatientSetupSequence: " + str(plan.PatientSetupSequence)
+    if patientSex:
+        print "PatientSex: " + plan.PatientSex
+    if RTPlanDate:
+        print "RTPlanDate: " + plan.RTPlanDate
+    if RTPlanGeomentry:
+        print "RTPlanGeometry: " + plan.RTPlanGeometry
+    if RTPlanLabel:
+        print "RTPlanLabel: " + plan.RTPlanLabel
+    if RTPlanName:
+        print "RTPlanName: " + plan.RTPlanName
+    if RTPlanTime:
+        print "RTPlanTime: " + plan.RTPlanTime
+    if RefdRTPlanSequence:
+        print " ---------------- RefdRTPlanSequence ----------------- "
+        print str(plan.RefdRTPlanSequence)
+    if RefdStructureSetSequence:
+        print " --------------- RefdStructureSetSequence ---------------- "
+        print str(plan.RefdStructureSetSequence)
+    if ReferencedRTPlanSequence:
+        print " ------------- Referenced RTPlanSequence -------------"
+        print str(plan.Referenced RTPlanSequence)
+    if ReferencedStructureSetSequence:
+        print " ------------- Referenced Structure Set Sequence ----------"
+        print str(plan.ReferencedStructureSetSequence)
+    if referencedPhysicianName:
+        print "ReferencedPhysicianName: " + plan.ReferencedPhysicianName
+    if SOPClassUID:
+        print "SOPClassUID: " + plan.SOPClassUID
+    if SOPInstanceUID:
+        print "SOPInstanceUID: " + plan.SOPInstanceUID
+    if seriesInstanceUID:
+        print "SeriesInstanceUID: " + plan.SeriesInstanceUID
+    if seriesNumber:
+        print "SeriesNumber: " + plan.SeriesNumber
+    if softwareVersions:
+        print "SoftwareVersions: " + plan.SoftwareVersions
+    if stationName:
+        print "StationName: " + plan.StationName
+    if studyDate:
+        print "StudyDate: " + plan.StudyDate
+    if studyID:
+        print "StudyID: " + plan.StudyID
+    if studyInstanceUID:
+        print "StudyInstanceUID: " + plan.StudyInstanceUID
+    if studyTime:
+        print "StudyTime: " + plan.StudyTime
 
 def edit_data_command(filepath, newfilepath="x", instanceCreationDate="", ):
     """ edit dicom Data by given Parameter
